97f9d88650173d9b2d398343e13b29fb4a3cd235
Ian Darwin <ian@darwinsys.com>
2022-11-26 21:02:24 -0500

    Another code sample: if,for,while

---
+++ lang/flow_control.dart
@@ -0,0 +1,19 @@
+main() {
+
+if (4 > 3) {
+    print("Great!");
+} else {
+    print("Not so hot");
+}
+for (int i = 0; i < 10; i++) {
+    print(i);
+}
+for (var j in ["alpha", "beta", 'delta', 'gamma']) {
+    print(j);
+}
+var i = 0;
+while (i < 10) {
+    print(i++);
+}
+
+}
