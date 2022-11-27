932e346aa6e46de9efecc091abb90d37c41d35f2
Ian Darwin <ian@darwinsys.com>
2022-11-26 20:16:26 -0500

    More examples

---
+++ lang/runtimetype.dart
@@ -0,0 +1,5 @@
+main(){
+print(x.runtimeType);
+print(3.14159.runtimeType);
+}
+String x = "Hello";
---
+++ lang/strings.dart
@@ -0,0 +1,7 @@
+main() {
+var names = [ "Ian", 'Robin', "Jo"];
+print(names.length); // prints "3"
+print(names.runtimeType); // prints "List"
+print(names[0].runtimeType);
+print(names);
+}
