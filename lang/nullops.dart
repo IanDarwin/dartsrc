8cad7865144c331518de0f07889509cb596db769
Ian Darwin <ian@darwinsys.com>
2022-12-02 15:36:07 -0500

    Expand example of null-safe ops

--- lang/nullops.dart
+++ lang/nullops.dart
@@ -1,20 +1,23 @@
 main() {
-Item? item = Item();
+Item? item = Item("Acme Widgets Anvil");
 String? detail;
 
 if (item != null) {
   detail = item.getProductDetail();
 }
-detail = item.getProductDetail(); // short for above
-
-detail = item.getProductDetail() ?? "(No details)";
-
-detail ??= "(No details)"; // Assign iff detail is null
-
+detail = item?.getProductDetail(); // short for above
 print(detail);
+detail = item.getProductDetail() ?? "(No details)";
+detail ??= "(Still no details)"; // Assign iff detail is null
+print(detail);
+print(item.length());
+print(detail!.length);
 }
 
 class Item {
-  String? getProductDetail() => "Acme Widget Anvil";
+  Item(this._detail);
+  final _detail;
+  String? getProductDetail() => _detail;
+  int length() => _detail.length;
 }
 
