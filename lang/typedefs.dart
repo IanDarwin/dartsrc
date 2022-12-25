ecfea889c5f2a1c29c3a8409cc13a39d346d13ee
Ian Darwin <ian@darwinsys.com>
2022-12-25 10:25:19 -0500

    Consistent naming

--- lang/typedefs.dart
+++ lang/typedefs.dart
@@ -3,16 +3,16 @@ typedef TwoNameFormatter(String first, String last);
 
 byFullName(String first, String last) => "$first $last";
 
-lastCommaFirst(first, last) => "$last, $first";
+byLastCommaFirst(first, last) => "$last, $first";
 
 main() {
 	// Format the name using byFullName, then lastCommaFirst
 	TwoNameFormatter formatter = byFullName;
 	print(formatter("Ian", "Darwin"));
-	formatter = lastCommaFirst;
+	formatter = byLastCommaFirst;
 	print(formatter("Ian", "Darwin"));
 	// Or even: apply multiple operations to same data
-	for (TwoNameFormatter fmt in [byFullName, lastCommaFirst]) {
+	for (TwoNameFormatter fmt in [byFullName, byLastCommaFirst]) {
 		print(fmt("Ian", "Darwin"));
 	}
 }
