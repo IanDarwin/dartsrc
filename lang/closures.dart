c7958c34632093ad1ded7abdd8e72e3f094a60a8
Ian Darwin <ian@darwinsys.com>
2022-12-25 11:28:50 -0500

    More examples

--- lang/closures.dart
+++ lang/closures.dart
@@ -3,4 +3,11 @@ main() {
 
   print(clos);
   print(clos('abc.123'));
+
+  found("the way out");
+  print(format('Shoes', 145.00));
 }
+
+void found(String s) => print("I found $s");
+
+String format(item, price) => "Item $item costs $price";
