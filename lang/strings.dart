b33ee7ec8e5b75c06de642c09eb01a77c1e02709
Ian Darwin <ian@darwinsys.com>
2023-01-01 20:18:29 -0500

    Repair indentation

--- lang/strings.dart
+++ lang/strings.dart
@@ -1,7 +1,7 @@
 main() {
-var names = [ "Ian", 'Robin', "Jo"];
-print(names.length); // prints "3"
-print(names.runtimeType); // prints "List"
-print(names[0].runtimeType);
-print(names);
+  var names = [ "Ian", 'Robin', "Jo"];
+  print(names.length); // prints "3"
+  print(names.runtimeType); // prints "List<String>"
+  print(names[0].runtimeType);
+  print(names);
 }
