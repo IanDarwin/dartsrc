19f2a68c9562da54c9f145c800710007dc4e629d
Ian Darwin <ian@darwinsys.com>
2023-01-07 12:26:45 -0500

    More String methods exposed

--- lang/strings.dart
+++ lang/strings.dart
@@ -1,7 +1,15 @@
 main() {
-  var names = [ "Ian", 'Robin', "Jo"];
+  var names = [ "Ian D", 'Robin S', "Jo J"];
+  print(names);
   print(names.length); // prints "3"
+  print(names[0].length); // prints "5"
   print(names.runtimeType); // prints "List<String>"
   print(names[0].runtimeType);
   print(names);
+  var ian = names[0];
+  print(ian.toLowerCase());
+  print(ian.split(''));
+  print(ian.codeUnits);
+  print(ian.runes);
+  print(ian.indexOf(" "));
 }
