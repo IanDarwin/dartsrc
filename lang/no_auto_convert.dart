80c8a986dd77206c30eaed208562f48084bd62ef
Ian Darwin <ian@darwinsys.com>
2023-01-29 22:36:54 +0300

    What it says on the tin

---
+++ lang/no_auto_convert.dart
@@ -0,0 +1,11 @@
+/// Shows an assigment most would expect to work, commented out
+/// so that this project will compile. Un-comment the assigment
+/// to watch dart failure in action.
+main() {
+
+	int i = 1;
+	double d = 0;
+	// d = i;		// FAIL - A value of type 'int' can't 
+	//		// be assigned to a variable of type 'double'
+	print(d);
+}
