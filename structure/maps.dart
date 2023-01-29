cd088cf047cde127948e3438411bb4873adb0d75
Ian Darwin <ian@darwinsys.com>
2023-01-29 22:35:53 +0300

    Document adding to a map

--- structure/maps.dart
+++ structure/maps.dart
@@ -5,8 +5,19 @@ print(scores['Ian']);
 
 // A more careful definition
 Map<String,int> scores2 =  { 'Ian':25, 'Linda':31, 'Robin': 27 };
+
+// What you would like to do:
+// scores2.add('Tobey', 34);
+// What you get if you try:
+// Error: The method 'add' isn't defined for the class 'Map'...
+// These two forms work:
 scores2.addAll({'Tobey': 34});
+scores2.putIfAbsent('Tobey', () => 34);
+
+// This works too:
 scores2['Boris'] = 19;
+
+// Collections can be iterated using forEach:
 scores2.forEach((key, value) {
   print('$key\t$value');
 });
