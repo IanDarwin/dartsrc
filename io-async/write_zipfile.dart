9950d18a95798dd963f6f08f5ff9033ef99a89f1
Ian Darwin <ian@darwinsys.com>
2023-03-22 18:50:53 -0400

    Write ZipFile

---
+++ io-async/write_zipfile.dart
@@ -0,0 +1,29 @@
+/// "Export" some files into a Zip file
+
+import 'dart:io';
+import 'package:archive/archive.dart';
+
+void main() {
+  // Create the ZIP archive
+  final archive = Archive();
+
+  // Input cane come from a file on dist
+  archive.addFile(
+    ArchiveFile('file1.txt', File('/tmp/name1.txt').lengthSync(), File('/tmp/name1.txt').readAsBytesSync()),
+  );
+  // Input can come from a String generated in the program
+  var generatedString = "Hello World\nWelcome to Dart!\n";
+  archive.addFile(
+    ArchiveFile('file2.txt', generatedString.length, generatedString.codeUnits),
+  );
+  // Input can be binary file and can go into a virtual subdirectory in the archive
+  var imageFileName = "/tmp/myface.png";
+  archive.addFile(
+    ArchiveFile('images/file3.png', File(imageFileName).lengthSync(), File(imageFileName).readAsBytesSync()),
+  );
+
+  // Save the ZIP archive to a file
+  final zipFile = File('archive.zip');
+  zipFile.writeAsBytesSync(ZipEncoder().encode(archive)!);
+}
+
